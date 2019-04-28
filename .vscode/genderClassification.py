691bf5392d6b94ebb38e6809592f3bd7b8f8432a
edbron <edbron411@gmail.com>
2019-04-28 21:32:17 +0000

    added NN model for same dataset

--- .vscode/genderClassification.py
+++ .vscode/genderClassification.py
@@ -1,10 +1,6 @@
 #import library
 from sklearn import tree
 
-#Classification model from scikit learn 
-#a decision tree to be presice 
-#because we want to classify into male of female
-
 #Defining our Data(List)
 #[shoe_size, height, dress_size]
 X = [[33, 157, 8], [37, 170, 10], [40, 181, 14], [36, 176, 8], [43, 179, 12], [44, 167, 14], [41, 185, 14], [37, 161, 10], [41, 166, 12], [32, 150, 6], [42, 178, 10] ]
