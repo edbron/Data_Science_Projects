accbe60b92a1413150091610f3c68dacb7d79f88
edbron <edbron411@gmail.com>
2019-04-29 16:09:25 +0000

    added a new file for sentiment analysis on twitter

---
+++ Scripts/TwitterSen_A.py
@@ -0,0 +1,26 @@
+#import library
+import tweepy
+from textblob import TextBlob
+
+#access tokens from twitter
+consumer_key = ''
+consumer_secret = ''
+
+accessToken = ''
+accessToken_secret = ''
+
+#creating our auth variable
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(accessToken, accessToken_secret)
+
+#assign our API
+api = tweepy.API(auth)
+
+#serching tweets
+public_tweets = api.search('')
+
+#performing the analysis
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
